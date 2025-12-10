9b39d22a2b0ae8eeb197fce209f4469f0dc85b76
thlibers <thlibers@student.42lehavre.fr>
2025-12-10 17:54:30 +0100

    Tres beau  commit, le pipex a ete sauver par Jhauvill

--- srcs/execution.c
+++ srcs/execution.c
@@ -6,23 +6,11 @@
 /*   By: thlibers <thlibers@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 14:22:08 by thlibers          #+#    #+#             */
-/*   Updated: 2025/12/10 16:35:09 by thlibers         ###   ########.fr       */
+/*   Updated: 2025/12/10 17:51:50 by thlibers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
-
-// static int find_last_cmd(int argc)
-// {
-// 	int	index;
-
-// 	index = 0;
-// 	while(index < argc - 2)
-// 	{
-// 		index++;
-// 	}
-// 	return (index);
-// }
 
 static void	first_last_command(t_pipex *pipex, int cmd_count)
 {
@@ -36,7 +24,7 @@ static void	first_last_command(t_pipex *pipex, int cmd_count)
 		close(pipex->pipe_fd[cmd_count][1]);
     	close(pipex->pipe_fd[cmd_count][0]);
 	}
-	else if (cmd_count == pipex->cmd_count)
+	else if (cmd_count == pipex->cmd_count - 1)
 	{
 		if (dup2(pipex->pipe_fd[cmd_count - 1][0], STDIN_FILENO) == -1)
 			print_error("dup2 failed for pipe read");
@@ -65,7 +53,7 @@ static void	child_process(t_pipex *pipex, int cmd_count)
 	char *cmd_path;
 	int	i;
 	
-	if (cmd_count == 0 && cmd_count == pipex->cmd_count - 1)
+	if (cmd_count == 0 || cmd_count == pipex->cmd_count - 1)
 		first_last_command(pipex, cmd_count);
 	else
 		setup_middle_command(pipex, cmd_count);
@@ -94,6 +82,7 @@ static void	child_process(t_pipex *pipex, int cmd_count)
 		cleanup_pipex(pipex);
 		exit(126);
 	}
+	printf("123\n");
 	free(cmd_path);
 }
 
@@ -101,21 +90,18 @@ void	execute_pipex(t_pipex *pipex)
 {
 	pid_t	*pid;
 	int		status;
-	// int		last_cmd_index;
 	int		i;
 
 	pid = NULL;
 	pid = malloc(sizeof(pid_t) * pipex->cmd_count);
 	if (!pid)
-		return ;
+		print_error("Allocation pid array failed");
 	pipex->pipe_fd = ft_calloc(pipex->cmd_count, sizeof(int *));
 	if (!pipex->pipe_fd)
-		print_error("Allocation Pipe fd failed");
+		print_error("Allocation Pipe_fd array failed");
 	i = 0;
 	while (i < pipex->cmd_count - 1)
 	{
-		if (i == 0 || i == pipex->cmd_count - 1)
-			first_last_command(pipex, i);
 		if (pipe(pipex->pipe_fd[i]) == -1)
 			print_error("Pipe creation failed");
 		i++;
@@ -145,5 +131,3 @@ void	execute_pipex(t_pipex *pipex)
 	}
 	free(pid);
 }
-
-// boucler le child_process pour executer les n fonctions
